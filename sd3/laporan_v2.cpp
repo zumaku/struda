c39b7cd3e0c50cf9d93650e7c192e5c44a4a40af
Zuma <116003626+zumaku@users.noreply.github.com>
2022-11-06 21:55:26 +0800

    Program Laporan 2
    Laporan 2 Praktikum
    Tanggal : 4 November 2022
    Judul : Structure
    
    Sebuah perusahaan membagi karyawan menjadi tiga golongan, yaitu golongan 1, 2 dan 3. Upah harian dan upah lembur perjam tiap-tiap golongan adalah:
    
    Golongan       Upah Harian        Upah Lembur
    1                     Rp. 30.000          Rp. 3.000
    2                     Rp. 40.000          Rp. 4.000
    3                     Rp. 50.000          Rp. 5.000
    
    Upah lembur akan diberikan jika pegawai bekerja lebih dari 8 jam. Buatlah program yang mneghitung total upah yang diterima karyawan perusahaan tersebut perharinya (Data masukan: Nama Karyawan, Golongan dan Jumlah Jam Kerja). Gunakan tipe data struktur untuk menyimpan data karyawan beserta upahnya.
    
    Buat agar program tersebut dapat digunakan berulang kali untuk menghitung upah harian dan upah lembur beberapa karyawan.
    
    Setelah pengimputan beberapa karyawan berakhir, tampilkan data karyawan yang memiliki total upah terbesar dan terkecil (Cetak nama dan total upah).

---
+++ sd3/laporan_v2.cpp
@@ -0,0 +1,115 @@
+// Program Total Upah Karyawan Tertinggi dan Terendah
+// Oleh Zul Fadli Ahmad
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// struktur karyawan
+struct karyawan{
+    string nama;
+    // char nama = 'a';
+    int golongan;
+    int jmlJK;
+    int upah;
+} karMin, karMax, kar;
+
+// fungsi pengecekan golongan
+void gol(){
+    if(kar.golongan < 1 || kar.golongan > 3){
+        cout<<"xx PERINGATAN!!!\nxx Golongan yang anda masukkan salah.\n";
+        cout<<"-> Masukkan Golongan Karyawan [1-3]: "; cin>>kar.golongan;
+        gol();
+    }
+}
+
+// global variable
+int i = 1;
+char lanjut;
+bool n = true;
+
+// fungsi lanjut mengisi karyawan
+void lanjutngk(){
+    if(lanjut == 'y'){
+        i++;
+        n = true;
+    } else if(lanjut == 't'){
+        cout<<"Pengisian data Selesai!\n";
+        n = false;
+    } else{
+        cout<<"PERINGATAN!!\n Inputan yang anda masukkan salah.\n";
+        cout<<"Data berhasil dimasukkan.\nIngin mengisih data lagi? [y/t]: "; cin>>lanjut;
+        lanjutngk();
+    }
+}
+
+// fungsi utama
+int main(){
+    cout<<"\nProgram Menghitung Upah Karyawan\n";
+    cout<<"================================\n";
+
+    while(n == true){
+        // input data
+        cout<<"\nInfo Karyawan ke-"<<i<<endl;
+        // cout<<"-> Masukkan Nama Karyawan: "; getline(cin, kar.nama);
+        cout<<"-> Masukkan Nama Karyawan: "; cin>>kar.nama;
+        cout<<"-> Masukkan Golongan Karyawan [1-3]: "; cin>>kar.golongan;
+        gol();  // pengecekan golongan apakah sesuai ketentuan 1-3
+        cout<<"-> Masukkan Jumlah Jam Kerja Karyawan: "; cin>>kar.jmlJK;
+
+        // penentuan upah
+        if(kar.golongan == 1){
+            kar.upah = 30000;
+            if(kar.jmlJK > 8){
+                kar.upah += 3000;
+            }
+        } else if(kar.golongan == 2){
+            kar.upah = 40000;
+            if(kar.jmlJK > 8){
+                kar.upah += 4000;
+            }
+        } else if(kar.golongan == 3){
+            kar.upah = 50000;
+            if(kar.jmlJK > 8){
+                kar.upah += 5000;
+            }
+        }
+
+        // penentuan upah tertinggi dan terendah
+        if(kar.upah > karMax.upah){
+            karMax.nama = kar.nama;
+            karMax.golongan = kar.golongan;
+            karMax.jmlJK = kar.jmlJK;
+            karMax.upah = kar.upah;
+        }
+
+        // penetapan nilai awal karMin
+        if(i==1){
+            karMin.upah = kar.upah;
+        }
+
+        //penentuan upah terendah
+        if(kar.upah <= karMin.upah){
+            karMin.nama = kar.nama;
+            karMin.golongan = kar.golongan;
+            karMin.jmlJK = kar.jmlJK;
+            karMin.upah = kar.upah;
+        }
+
+        // Pemilihan masih mau isi data atau tidak
+        cout<<"== Data berhasil dimasukkan!\n\nIngin mengisih data lagi? [y/t]: "; cin>>lanjut;
+        lanjutngk();
+    }
+
+    // menampilkan karyawan dengan nilai tertinggi dan terendah
+    cout<<"\n\nKaryawan dengan Upah tertinggi.\n";
+    cout<<"Nama: "<<karMax.nama<<endl;
+    cout<<"Upah: "<<karMax.upah<<endl;
+
+    cout<<"\nKaryawan dengan Upah terendah.\n";
+    cout<<"Nama: "<<karMin.nama<<endl;
+    cout<<"Upah: "<<karMin.upah<<endl;
+
+    cin.get();
+    return 0;
+}

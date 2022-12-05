28cf6cb393ebb61d15e1564934136324a6107a9b
Zuma <116003626+zumaku@users.noreply.github.com>
2022-12-05 14:41:33 +0800

    Update laporan.cpp

--- sd7/laporan.cpp
+++ sd7/laporan.cpp
@@ -9,43 +9,40 @@ struct Angka{
 Angka *current, *head, *tail = NULL, *newnode;
 int i = 1, n;
 
-// void buatNode(int n){
-//     head = new Angka();
-//     head->angka = n;
-
-//     if(i == 1){ head->next = NULL; }
-//     else{ head->next = tail; }
-
-//     tail = head;
-//     i++;
-// }
 
 // untuk pembuatan node awal, sekaligus menjadi tail
 void buatNode(int n){
     head = new Angka();
     head->angka = n;
     head->next = tail;
-    tail = head;
+    if(head->next == NULL){
+        newnode = new Angka();
+        newnode->angka =  head->angka;
+        newnode->next = head;
+        tail = newnode;
+    } else{
+        tail = head;
+    }
 }
+
 
 // fungsi cetak node
 void cetakNode(){
-    // current = tail;
     current = head;
-    while(current != NULL){
+    while(current->next != NULL){
         cout<<current->angka<<" --> ";
         current = current->next;
     }
-    if(current == NULL){
+    if(current->next == NULL){
         cout<<"NULL"<<endl;
     }
 }
 
+
 // fungsi sorting
 void sorting(){
-    Angka *i, *j;
+    Angka *i, *j, *cur;
     int temp;
-
     for(i=head; i->next != NULL; i = i->next){
         for(j=i->next; j->next != NULL; j = j->next){
             if(i->angka > j->angka){
@@ -54,23 +51,18 @@ void sorting(){
                 j->angka = temp;
             }
         }
+        
     }
 }
-
-
 
 
 int main(){
     system("CLS");
     int n;
 
-    // cout<<"Masukkan angka: ";
-    // cin>>n;
-    // buatNode(n);
     do{
         cout<<"Masukkan angka: ";
         cin>>n;
-        // tambahNode(n);
         buatNode(n);
     } while(n != 0);
     cetakNode();

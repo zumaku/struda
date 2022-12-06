8d5ecec6dbd0ee743c663d6207a2d3e522755f70
Zuma <116003626+zumaku@users.noreply.github.com>
2022-12-06 20:53:58 +0800

    Sudah bisa menghapus, tapi yg ganjil yang dihapus

--- sd7/laporan.cpp
+++ sd7/laporan.cpp
@@ -1,3 +1,9 @@
+// LAPORAN PRAKTIKUM 5
+/*
+1. Buat program untuk menginput data bilangan bulat sebara acak, kemudian masukkan data tersebut kedalam sebuah linked list dengan berurut secara ascending (misalnya data yang diinput: 4, 8, 2, 9, 5, 6. Maka hasil setelah dimasukkan ke dalam lingked list adalah: 2 4 5 6  8 9).
+2. Untuk soal nomer 1 diatas, setelah selesai dilakukan kemudian hapus semua data bilangan yang genap saja sehingga isi dari lingked list hanya data bilangan bulat yang ganjil saja (misalnya contoh diatas maka isi terakhir dari lingked list adalah: 5 dan 9).
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -10,17 +16,19 @@ Angka *current, *head, *tail = NULL, *newnode;
 int i = 1, n;
 
 
-// untuk pembuatan node awal, sekaligus menjadi tail
+// untuk pembuatan node
 void buatNode(int n){
     head = new Angka();
     head->angka = n;
     head->next = tail;
     if(head->next == NULL){
+        // Kondisi ini hanya terjadi sekali, yaitu pada saat elemen pertama, tujuannya agar data yang menunjuk ke NULL juga dapat di sorting. Angka pada elemen pertama akan di simpan ke node baru yang menunjuk ke head.
         newnode = new Angka();
         newnode->angka =  head->angka;
         newnode->next = head;
         tail = newnode;
     } else{
+        // Setelah elemen pertama terisi, maka sisanya hanya akan berjalan normal
         tail = head;
     }
 }
@@ -41,7 +49,7 @@ void cetakNode(){
 
 // fungsi sorting
 void sorting(){
-    Angka *i, *j, *cur;
+    Angka *i, *j;
     int temp;
     for(i=head; i->next != NULL; i = i->next){
         for(j=i->next; j->next != NULL; j = j->next){
@@ -51,7 +59,41 @@ void sorting(){
                 j->angka = temp;
             }
         }
-        
+    }
+}
+
+// fungsi menghapus nilai genap
+void delGenap(){
+    Angka *temp = head;
+    Angka *prev = head;
+
+    if(temp->angka == 0){   // ini untuk membuat node.angka yg isinya 0 (yaitu pada nide pertama(head)) menjadi 2, agar dapat dimodulus 2 pada line berikutnya
+        temp->angka = 2;
+    }
+
+    // while(temp->next != NULL){
+    //     if(temp->angka % 2 == 0){
+    //         prev->next = temp->next;
+    //         free(temp);
+    //         Angka *temp;
+    //         temp = prev->next;
+    //     } else{
+    //         prev = temp;
+    //         temp = temp->next;
+    //     }
+    // }
+
+
+    // perulangan dibawah ini berhasil, tapi menampilkan hanya yang bernilai genap 
+    while(temp->next != NULL){
+        if(temp->angka % 2 == 0){
+            prev = temp;
+            temp = temp->next;
+        } else{
+            prev->next = temp->next;
+            free(temp);
+            temp = prev->next;
+        }
     }
 }
 
@@ -68,6 +110,9 @@ int main(){
     cetakNode();
 
     sorting();
+    cetakNode();
+
+    delGenap();
     cetakNode();
 
     return 0;
